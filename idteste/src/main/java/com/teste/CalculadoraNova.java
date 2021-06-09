82e8c5b3562d4726815ff118b224a96be4c507ee
IzaqueL95 <izaque_14@live.com>
2021-06-09 16:39:02 -0300

    atualização de arquivos de testes

--- idteste/src/main/java/com/teste/CalculadoraNova.java
+++ idteste/src/main/java/com/teste/CalculadoraNova.java
@@ -2,8 +2,13 @@ package com.teste;
 
 public class CalculadoraNova {
 	
-	public int somar(int valorA, int valorB) {
-		return 0;
+	public int somar(int...valores) {
+		int soma = 0;
+		for(int valor: valores) {
+			soma += valor;
+			
+		}
+		return soma;
 	}
 
 }
--- idteste/src/test/java/com/teste/CalculadoraNovaTest.java
+++ idteste/src/test/java/com/teste/CalculadoraNovaTest.java
@@ -14,9 +14,29 @@ public class CalculadoraNovaTest {
 		
 		//int soma = 0;
 		
-		CalculadoraNova calc = new CalculadoraNova()
+		CalculadoraNova calc = new CalculadoraNova();
+		
+		int soma = calc.somar(valorA, valorB);
+		
 		
 		assertEquals(3, soma);
+		
+	}
+	
+	@Test
+	public void somaTres(){
+		
+		int valorA = 1;
+		int valorB = 2;
+		int valorC = 3;
+		//int soma = 0;
+		
+		CalculadoraNova calc = new CalculadoraNova();
+		
+		int soma = calc.somar(valorA, valorB, valorC);
+		
+		
+		assertEquals(6, soma);
 		
 	}
 
